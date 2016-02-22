dbf8629f638b0223122bde647c9f2a86e51f900f
rachidasen <rachidasen@users.noreply.github.com>
2016-02-22 21:47:08 +0530

    Update f.sh
    you can install and configure rtorrent

--- f.sh
+++ f.sh
@@ -1,7 +1,10 @@
+apt-get install rtorrent
 mkdir /home/$USER/rDownloads
 mkdir /home/$USER/session
 apt-get install unzip
 wget -O r.zip https://gist.github.com/rachidasen/dcdfc2939862b333ddcc/archive/bf0d3dd904b62af0345ff7564bbb44ee58eda024.zip && unzip r.zip && rm r.zip
 cp dcdfc2939862b333ddcc-bf0d3dd904b62af0345ff7564bbb44ee58eda024/.rtorrent.rc /home/$USER/ && rm -rf  dcdfc2939862b333ddcc-bf0d3dd904b62af0345ff7564bbb44ee58eda024
+
+
 
 
